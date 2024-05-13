7e5ef6ed90f2428ec491250f8b4ce32f00072c22
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-05-13 12:32:04 +0330

    Initial Commit

---
+++ ButtonAndSwitch/ButtonAndSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonAndSwitchApp.swift
+//  ButtonAndSwitch
+//
+//  Created by Omid Heydarzadeh on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonAndSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
